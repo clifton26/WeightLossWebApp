fc567b9ae079c977135a50e33b30ec0c5aa6a1ae
Diogo <diogomartins5@hotmail.com>
2016-05-20 19:50:46 +0100

    Registo alterado ;) !

--- src/WebApplication4/ViewModels/Account/RegisterViewModel.cs
+++ src/WebApplication4/ViewModels/Account/RegisterViewModel.cs
@@ -14,6 +14,11 @@ namespace WebApplication4.ViewModels.Account
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
@@ -23,5 +28,34 @@ namespace WebApplication4.ViewModels.Account
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(18, 99, ErrorMessage = "Please enter a valid number!")]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Height")]
+        public int Height { get; set; }
+
+        [Required]
+        [Display(Name = "Weight")]
+        public int Weight { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Sex")]
+        public string Sex { get; set; }
+
+
+        public List<string> getSexOptions()
+        {
+            List<string> sexOptions = new List<string>();
+            sexOptions.Add("Male");
+            sexOptions.Add("Female");
+
+            return sexOptions;
+        }
+
     }
 }
