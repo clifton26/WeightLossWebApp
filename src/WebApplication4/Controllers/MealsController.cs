139e324642667b0af3ba3c6845e53fcef8736b0f
Diogo <diogomartins5@hotmail.com>
2016-06-28 02:21:04 +0100

    NOW?

--- src/WebApplication4/Controllers/MealsController.cs
+++ src/WebApplication4/Controllers/MealsController.cs
@@ -114,9 +114,9 @@ namespace WebApplication4.Controllers
 
                 newCalculator.Grams = 100 * int.Parse(quantity.Substring(0, quantity.Length - 1));
 
-                newCalculator.Lipid = int.Parse(lipids.Substring(0, lipids.Length - 1));
+                newCalculator.Lipid = int.Parse(lipids.Replace(".", ""));
 
-                newCalculator.Calories = int.Parse(calories.Substring(0, calories.Length - 1));
+                newCalculator.Calories = int.Parse(calories.Replace(".", ""));
 
                 _context.FoodCalculator.Add(newCalculator);
 
